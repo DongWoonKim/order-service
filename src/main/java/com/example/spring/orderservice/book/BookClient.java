2b4c30b82eafa3b8e2221353eb3fc8b6adb8315c
DongWoonKim <01030985843z@naver.com>
2024-10-22 11:41:55 +0900

    order service

---
+++ src/main/java/com/example/spring/orderservice/book/BookClient.java
@@ -0,0 +1,23 @@
+package com.example.spring.orderservice.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class BookClient {
+
+    private static final String BOOKS_ROOT_API = "/books/";
+    private final WebClient webClient;
+
+    public Mono<Book> getBookByIsbn(String isbn) {
+        return webClient
+                .get()
+                .uri(BOOKS_ROOT_API + isbn)
+                .retrieve() // 요청을 보내고 응답을 기다린다.
+                .bodyToMono(Book.class);
+    }
+
+}
--- src/main/java/com/example/spring/orderservice/order/domain/OrderService.java
+++ src/main/java/com/example/spring/orderservice/order/domain/OrderService.java
@@ -1,5 +1,7 @@
 package com.example.spring.orderservice.order.domain;
 
+import com.example.spring.orderservice.book.Book;
+import com.example.spring.orderservice.book.BookClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -8,6 +10,8 @@ import reactor.core.publisher.Mono;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+
+    private final BookClient bookClient;
     private final OrderRepository orderRepository;
 
     public Flux<Order> getAllOrders() {
@@ -15,15 +19,27 @@ public class OrderService {
     }
 
     public Mono<Order> submitOrder(String isbn, int quantity) {
-        return Mono.just(buildRejectedOrder(isbn, quantity))
+        return bookClient.getBookByIsbn(isbn)
+                .map(book -> buildAcceptedOrder(book, quantity))
+                .defaultIfEmpty(buildRejectedOrder(isbn, quantity))
                 .flatMap(orderRepository::save);
+    }
+
+    public static Order buildAcceptedOrder(Book book, int quantity) {
+        return Order.builder()
+                .bookIsbn(book.isbn())
+                .bookName(book.title() + " - " + book.author())
+                .bookPrice(book.price())
+                .quantity(quantity)
+                .status(OrderStatus.ACCEPTED)
+                .build();
     }
 
     public static Order buildRejectedOrder(String isbn, int quantity) {
         return Order.builder()
                 .bookIsbn(isbn)
                 .quantity(quantity)
-                .status(OrderStatus.REJECTED)
+                .status(OrderStatus.ACCEPTED)
                 .build();
     }
 
